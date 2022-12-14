aea88c99285bd63dd650db8f458de081e4592fd5
fellowbeard <virgovibe@gmail.com>
2022-12-14 10:40:42 -0600

    adds update, destroy, and routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   delete '/api-keys', to: 'api_keys#destroy'
   get '/api-keys', to: 'api_keys#index'
 
-  patch '/votes/:id', to: 'votes#update'
   post '/votes', to: 'votes#create'
   delete '/votes', to: 'votes#destroy'
   get '/votes', to: 'votes#index'
   get '/votes/:id', to: 'votes#show'
+  patch '/votes/:id', to: 'votes#update'
   
   post '/locations', to: 'locations#create'
   delete '/locations', to: 'locations#destroy'
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get '/locations/:id', to: 'locations#show'
   patch '/locations/:id', to: 'locations#update'
   
+  post '/users', to: 'user#create'
+  delete '/users', to: 'user#destroy'
+  get '/users', to: 'users#index'
+  patch '/users/:id', to: 'users#update'
 
   
 end
