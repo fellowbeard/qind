92c0f8af0712bceb2422c19e1bbaa08912a772b2
Michael Beard <95155164+fellowbeard@users.noreply.github.com>
2023-02-15 19:51:42 -0600

    adds create, show, update, destroy methods for locations (#3)
    * adds create, show, update, destroy methods for locations
    
    * resolves conflicts, updates spelling
    
    * secret message

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,18 +1,35 @@
 class LocationsController < ApplicationController
   def index
-    render json: Location.all
+    locations = Location.all
+    render json: locations
   end
-
+  # this thing here is stupid
   def create
+    location = Location.new(
+      name: params[:name],
+      address: params[:address]
+    )
+    location.save
+    render json: location
   end
 
   def show
+    location = Location.find_by(id: params[:id])
+    render json: location
   end
 
   def update
+    location = Location.find_by(id: params[:id])
+    location.name = params[:name] || location.name
+    lcoation.address = params[:address] || location.address
+    location.save
+    render json: location.as_json
   end
 
   def destroy
+    location = Location.find_by(id: params[:id])
+    location.destroy
+    render json: { message: "Location succesfully destroyed" }
   end
 
 end
