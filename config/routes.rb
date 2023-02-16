4c8b8a7e1d91ac88e035d8848b69c4e47629a79f
Michael Beard <95155164+fellowbeard@users.noreply.github.com>
2023-02-15 20:38:24 -0600

    Apitesting (#4)
    * add enum gem
    
    * updates api junk

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   get '/api-keys', to: 'api_keys#index'
 
   post '/votes', to: 'votes#create'
-  delete '/votes', to: 'votes#destroy'
   get '/votes', to: 'votes#index'
   get '/votes/:id', to: 'votes#show'
   patch '/votes/:id', to: 'votes#update'
+  delete '/votes/:id', to: 'votes#destroy'
   
   post '/locations', to: 'locations#create'
-  delete '/locations', to: 'locations#destroy'
   get '/locations', to: 'locations#index'
   get '/locations/:id', to: 'locations#show'
   patch '/locations/:id', to: 'locations#update'
+  delete '/locations/:id', to: 'locations#destroy'
   
-  post '/users', to: 'user#create'
-  delete '/users', to: 'user#destroy'
+  post '/users', to: 'users#create'
   get '/users', to: 'users#index'
   patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 
   
 end
